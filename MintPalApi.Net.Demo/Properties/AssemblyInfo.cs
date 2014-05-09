8902d3fc99f72f3a778aa620852ac634ffb6bfe2
Jojatekok <jojatekokweb@gmail.com>
2014-05-09 08:51:29 +0200

    Made the icon's text more readable at smaller resolutions
    Also, incremented the demo application's version (I forgot to do it in
    my previous 2 commits).

--- MintPalApi.Net.Demo/Properties/AssemblyInfo.cs
+++ MintPalApi.Net.Demo/Properties/AssemblyInfo.cs
@@ -45,7 +45,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0")]
-[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyVersion("0.3.0")]
+[assembly: AssemblyFileVersion("0.3.0")]
 [assembly: GuidAttribute("689f4188-2f24-43d4-8bb4-97219973ddf2")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
