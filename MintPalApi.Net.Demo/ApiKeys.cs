bd793f94e9d9631896e2762d6ca1b7a9915180c8
Jojatekok <jojatekokweb@gmail.com>
2014-05-16 18:44:21 +0200

    Added an advice about sensitive data protection

--- MintPalApi.Net.Demo/ApiKeys.cs
+++ MintPalApi.Net.Demo/ApiKeys.cs
@@ -2,8 +2,9 @@
 {
     struct ApiKeys
     {
-        // The changes of this file will not be exposed to git
-        // Do not worry about entering sensitive data here!
+        // WARNING: In order not to expose the changes of this file to git, please 
+        //          run the following command every time you clone the repository:
+        // git update-index --assume-unchanged "MintPalApi.Net.Demo\ApiKeys.cs"
 
         internal const string PublicKey = "";
         internal const string PrivateKey = "";
